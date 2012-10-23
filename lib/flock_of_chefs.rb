c58c79a218c2a69ec5b2657616c06926d9940ca2
Chris Roberts <chrisroberts.code@gmail.com>
2012-10-23 00:37:47 -0700

    Only load if we are within a supported version

--- lib/flock_of_chefs.rb
+++ lib/flock_of_chefs.rb
@@ -1,2 +1,8 @@
+require 'chef'
+
+if(Chef::Version.new(Chef::VERSION) < Chef::Version.new('10.16.0'))
+  raise 'Unsupported version of Chef. 10.16.0 or greater required!'
+end
+
 require 'flock_of_chefs/version'
 require 'flock_of_chefs/loader'
