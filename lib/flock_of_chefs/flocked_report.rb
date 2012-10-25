87b6671089ffc0e1e2c3fbe275ce60d244c62c3e
Chris Roberts <chrisroberts.code@gmail.com>
2012-10-24 20:26:05 -0700

    Update handler. Run at start and end

--- lib/flock_of_chefs/flocked_report.rb
+++ lib/flock_of_chefs/flocked_report.rb
@@ -1,49 +1,93 @@
 require 'chef/handler'
 
 module FlockOfChefs
+
   class FlockedReport < Chef::Handler
-    def report
-      if(DCell.me.nil?)
-        setup_dcell!
+    def flocker(currently_active)
+      unless(DCell.me)
+        FlockOfChefs.start_flocking!(node)
       end
       dnode = DCell.me
       if(dnode)
-        dnode[:flock_api].node = node.to_hash
+        dnode[:flock_api].node = node
+        dnode[:flock_api].active = currently_active
         Chef::Log.info 'Node information successfully stored in flock'
       else
         Chef::Log.warn 'Failed to store node information in flock!'
       end
     end
+  end
 
-    def setup_dcell!
+  class StartConverge < FlockedReport
+    def report
+      flocker(true)
+    end
+  end
+
+  class StopConverge < FlockedReport
+    def report
+      flocker(false)
+    end
+  end
+
+  class << self
+    def find_flock_bind_addr(node)
+      if(i = node[:flock_of_chefs][:bind_addr][:device])
+        node.network.interfaces.send(i).addresses.keys[1]
+      elsif(node[:flock_of_chefs][:bind_addr][:ip_address])
+        node[:flock_of_chefs][:bind_addr][:ip_address]
+      else
+        node.ip_address
+      end
+    end
+
+    def find_flock_registry_addr(node, registry_node)
+      if(node[:flock_of_chefs][:registry][:ip_address])
+        node[:flock_of_chefs][:registry][:ip_address]
+      else
+        registry_node.ip_address
+      end
+    end
+
+    def start_flocking!(node)
       raise 'Flock of chefs cookbook not in use!' unless node[:flock_of_chefs]
+      bind_addr = find_flock_bind_addr(node)
       if(node.recipes.include?('flock_of_chefs::keeper'))
-        # NOTE: just gossip for now, zookeeper later
+        # We are the keeper!
         start_args = {
           :node => node.name,
-          :addr => "tcp://#{node[:ipaddress]}:#{node[:flock_of_chefs][:port]}"
+          :addr => "tcp://#{bind_addr}:#{node[:flock_of_chefs][:port]}"
         }
+        # TODO: Add in the other adapters
         case node[:flock_of_chefs][:registry][:type].to_s
         when 'zk'
-          require 'dcell/registries/zk'
-          start_args.merge(
+          require 'dcell/registries/zk_adapter'
+          start_args.merge!(
             :registry => {
               :adapter => 'zk',
-              :host => '127.0.0.1', # allow non-local?
-              :port => 2181
+              :server => '127.0.0.1:2181'
+            }
+          )
+        else
+          start_args.merge!(
+            :registry => {
+              :adapter => 'gossip'
             }
           )
         end
         DCell.start(start_args)
       else
-        keeper_node = Chef::Search::Query.new.search(:node, 'recipes:flock_of_chefs\:\:keeper').first.first
+        keeper_node = Chef::Search::Query.new.search(:node, 
+          'recipes:flock_of_chefs\:\:keeper'
+        ).first.first
+        registry_ip = find_flock_registry_addr(node, keeper_node)
         raise 'Failed to find flock keeper!' unless keeper_node
         DCell.start(
           :node => node.name,
-          :addr => "tcp://#{node[:ipaddress]}:#{node[:flock_of_chefs][:port]}",
+          :addr => "tcp://#{bind_addr}:#{node[:flock_of_chefs][:port]}",
           :directory => {
             :id => keeper_node.name,
-            :address => "tcp://#{keeper_node[:ipaddress]}:#{keeper_node[:flock_of_chefs][:port]}"
+            :addr => "tcp://#{registry_ip}:#{keeper_node[:flock_of_chefs][:port]}"
           }
         )
       end
