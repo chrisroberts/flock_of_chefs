ce5369d1a4f1ec9579ee0c8d11cf46c6287d42d7
Chris Roberts <chrisroberts.code@gmail.com>
2012-10-26 10:09:55 -0700

    Keep actors alive. Don't let exceptions bubble up.

--- lib/flock_of_chefs/actors/flock_api.rb
+++ lib/flock_of_chefs/actors/flock_api.rb
@@ -17,13 +17,25 @@ module FlockOfChefs
     end
 
     def run_chef
-      chef_app.run_chef_client
+      begin
+        chef_app.run_chef_client
+        true
+      rescue
+        false
+      end
     end
 
     def node(attribute_string=nil)
       if(attribute_string)
-        attribute_string.to_s.split('.').delete_if(&:empty?).inject(@node) do |memo,arg|
-          memo.send(arg)
+        begin
+          attribute_string.to_s.split('.').delete_if(&:empty?).inject(@node) do |memo,arg|
+            memo.send(arg)
+          end
+        rescue ArgumentError
+          nil
+        rescue => e
+          Chef::Log.warn "FlockAPI error: #{e.class}: #{e}\n#{e.backtrace.join("\n")}"
+          nil
         end
       else
         node_hash
