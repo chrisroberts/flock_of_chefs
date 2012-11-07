aa970c0241a772a8406fc8f781e6d0e2d121863d
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-06 18:17:54 -0800

    Hook into run_action to allow sending immediate notifications

--- lib/flock_of_chefs/monkey_bag/flocked_runner.rb
+++ lib/flock_of_chefs/monkey_bag/flocked_runner.rb
@@ -2,10 +2,18 @@ module FlockOfChefs
   module FlockedRunner
     def flocked_init(*args)
       original_initialize(*args)
-      if(DCell.me)
-        DCell.me[:flock_api].active = true
-        DCell.me[:resource_manager].new_run(self)
+      if(FlockOfChefs.me)
+        FlockOfChefs.me[:flock_api].active = true
+        FlockOfChefs.get(:resource_manager).new_run(self)
       end
+    end
+
+    def flocked_run_action(*args)
+      original_run_action(*args)
+      Chef::Log.info "Sending immediate flock notifications for #{args.first.resource_name}[#{args.first.name}]"
+      FlockOfChefs.get(:resource_manager).send_notifications(
+        args[0], args[1], :immediately
+      )
     end
 
     class << self
@@ -13,6 +21,8 @@ module FlockOfChefs
         base.class_eval do
           alias_method :original_initialize, :initialize
           alias_method :initialize, :flocked_init
+          alias_method :original_run_action, :run_action
+          alias_method :run_action, :flocked_run_action
         end
       end
     end
