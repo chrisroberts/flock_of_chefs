bcdf24974b77e30e04d368809687409f4da3d3e7
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-06 18:18:15 -0800

    Register notifications and subscriptions via resource manager

--- lib/flock_of_chefs/monkey_bag/flocked_resources.rb
+++ lib/flock_of_chefs/monkey_bag/flocked_resources.rb
@@ -1,37 +1,26 @@
-# NOTE: loc_hash support starts with :node only!
-# NOTE: we need to add timing for notification/subscriptions. i think 
-#   we can do this by creating new block resources and throwing them at the
-#   end of the current run context for delayed
 module FlockOfChefs
   module FlockedResources
-    def flocked_init(*args)
-      super
-      dnode = DCell.me
-      if(dnode)
-        dnode[:resource_manager].register_resource(self)
-      end
-    end
 
-    def flocked_run_action(*args)
-      original_run_action(*args)
-      if(updated_by_last_action? && DCell.me)
-        DCell.me[:resource_manager].notify_subscribers(self)
-      end
-    end
-
-    def remote_subscribes(action, resource, loc_hash={})
+    def remote_subscribes(action, resource, *args)
+      loc_hash = args.detect{|elm| elm.is_a?(Hash)}
+      raise TypeError.new 'Location hash is required!' unless loc_hash
+      args.delete(loc_hash)
+      timing = args.first || :immediately
       r_type,r_name = break_resource_string(resource)
-      DCell::Node[loc_hash[:node]][:resource_manager].subscription(
-        DCell.me.id, r_type, r_name, 
-        convert_to_snake_case(self.class), 
-        name, action
+      # TODO: Update this to accept discovery based attributes instead of just 'node'
+      FlockOfChefs.get(:resource_manager).send_subscribe_to_resource(
+        loc_hash, self, r_type, r_name, action, timing
       )
     end
 
-    def remote_notifies(action, resource, loc_hash={})
+    def remote_notifies(action, resource, *args)
+      loc_hash = args.detect{|elm| elm.is_a?(Hash)}
+      raise TypeError.new 'Location hash is required!' unless loc_hash
+      args.delete(loc_hash)
+      timing = args.first || :immediately
       r_type,r_name = break_resource_string(resource)
-      DCell::Node[loc_hash[:node]][:resource_manager].notify_resource(
-        r_type, r_name, action
+      FlockOfChefs.get(:resource_manager).register_notification(
+        loc_hash, self, r_type, r_name, action, timing
       )
     end
 
@@ -40,20 +29,8 @@ module FlockOfChefs
       resource.scan(/([^\[]+)\[([^\]]+)\]/).flatten
     end
 
-    class << self
-      def included(base)
-        base.class_eval do
-          alias_method :original_run_action, :run_action
-          alias_method :run_action, :flocked_run_action
-        end
-      end
-    end
   end
 end
-
-# Make all resources actors
-# TODO: Give them wait staff jobs while they wait for their big break
-#Chef::Resource.send(:include, Celluloid)
 
 # Hook in custom functionality to all resources
 [
