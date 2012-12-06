aaec04d6a9a9e1c46b7502c643ec27a65e1976fe
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-05 22:24:57 -0800

    Fix up resource manager to properly send and receive notifications

--- lib/flock_of_chefs/actors/resource_manager.rb
+++ lib/flock_of_chefs/actors/resource_manager.rb
@@ -4,12 +4,10 @@ module FlockOfChefs
   class ResourceManager
     include Celluloid
 
-    attr_accessor :runner
+    attr_accessor :notifications
 
     def initialize
-      @stale_resources = {}
-      @subscriptions = {}
-      @notifications = {}
+      @notifications = Mash.new
       @runner = nil
       find_existing_runner!
     end
@@ -24,9 +22,10 @@ module FlockOfChefs
 
     # sending to remote
     #
-    def send_subscribe_to_resource(loc_res, r_type, r_name, action, timing)
-      determine_remote_nodes(loc_res).each do |r_node|
-        r_node.register_subscription(
+    def send_subscribe_to_resource(loc_hash, loc_res, r_type, r_name, action, timing)
+      loc_hash = Mash.new(loc_hash)
+      determine_remote_nodes(loc_hash).each do |r_node|
+        r_node[:resource_manager].register_subscription(
           FlockOfChefs.me.id, r_type, r_name, loc_res.resource_name,
           loc_res.name, action, timing
         )
@@ -39,13 +38,14 @@ module FlockOfChefs
     end
 
     def register_notification(loc_hash, l_resource, r_type, r_name, action, timing)
-      add_to_notitfications(loc_hash, l_resource.resource_name, l_resource.name, r_type, r_name, action, timing)
+      loc_hash = Mash.new(loc_hash)
+      add_to_notifications(loc_hash, l_resource.resource_name, l_resource.name, r_type, r_name, action, timing)
     end
 
     def notify_collection(collection, action)
       collection.each do |noti|
         determine_remote_nodes(noti[:location_hash]).each do |r_node|
-          r_node.receive_notification(
+          r_node[:resource_manager].receive_notification(
             noti[:remote_type],
             noti[:remote_name],
             action
@@ -58,10 +58,11 @@ module FlockOfChefs
     #
 
     def register_subscription(node_id, l_type, l_name, r_type, r_name, action, timing)
-      add_to_notitfications({:node => node_id}, l_type, l_name, r_type, r_name, action, timing)
+      add_to_notifications(Mash.new(:node => node_id), l_type, l_name, r_type, r_name, action, timing)
     end
 
     def receive_notification(res_type, res_name, action)
+      Chef::Log.info "Received remote notification for: #{res_type}[#{res_name}] - #{action}"
       resource = @runner.run_context.resource_collection.lookup("#{res_type}[#{res_name}]")
       if(resource)
         @runner.run_action(resource, action)
@@ -78,14 +79,16 @@ module FlockOfChefs
     # action:: Action to run on remote resource
     # timiming:: Timing of notification (:immediately or :delayed)
     def add_to_notifications(loc_hash, l_type, l_name, r_type, r_name, action, timing)
-      @notifications[l_type] ||= {}
-      @notifications[l_type][l_name] ||= {}
-      @notifications[l_type][l_name][action] ||= {}
+      @notifications[l_type] ||= Mash.new
+      @notifications[l_type][l_name] ||= Mash.new
+      @notifications[l_type][l_name][action] ||= Mash.new
       @notifications[l_type][l_name][action][timing] ||= []
       @notifications[l_type][l_name][action][timing].push(
-        :remote_type => r_type,
-        :remote_name => r_name,
-        :location_hash => loc_hash
+        Mash.new(
+          :remote_type => r_type,
+          :remote_name => r_name,
+          :location_hash => loc_hash
+        )
       )
       @notifications[l_type][l_name][action][timing].uniq!
     end
