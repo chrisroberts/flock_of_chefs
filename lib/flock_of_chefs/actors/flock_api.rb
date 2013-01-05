2cd49dc0ca056fdcac9071a99827532e397a5513
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-05 10:04:04 -0800

    Provide subscription discovery point

--- lib/flock_of_chefs/actors/flock_api.rb
+++ lib/flock_of_chefs/actors/flock_api.rb
@@ -4,18 +4,25 @@ module FlockOfChefs
 
     attr_reader :active
 
+    # app:: Chef::Application instance
+    # Set chef app
     def chef_app=(app)
       @chef_app = app
     end
 
+    # node:: Chef::Node
+    # Set chef node
     def node=(node)
       @node = node
     end
-   
+  
+    # bool:: Boolean
+    # Set if chef is currently running
     def active=(bool)
       @active = !!bool
     end
 
+    # Perform a chef run
     def run_chef
       begin
         Thread.new do
@@ -27,6 +34,8 @@ module FlockOfChefs
       end
     end
 
+    # attribute_string:: Period delimited attribute key
+    # Return node hash or attribute based on passed key
     def node(attribute_string=nil)
       if(attribute_string)
         begin
@@ -42,12 +51,35 @@ module FlockOfChefs
       end
     end
 
+    # Returns full node Hash
     def node_hash
-      node.to_hash
+      Mash.new(node.to_hash)
     end
 
+    # Returns raw node. Only useful internally on given node.
     def raw_node
       @node
+    end
+
+    # node_info: Mash of node data cut down
+    def discovery_notifications(node_info)
+      matches = []
+      raw_node[:flock_of_chefs][:subscriptions].each do |loc_hash, sub|
+        match = false
+        if(loc_hash[:name])
+          match = node_info[:name] == loc_hash[:node]
+        end
+        [:roles, :recipes].each do |key|
+          if(loc_hash[key])
+            match = !(Array(node_info[key]) & Array(loc_hash[key])).empty?
+          end
+        end
+        if(loc_hash[:environment])
+          match = loc_hash[:environment] == node_info[:chef_environment]
+        end
+        matches << sub if match
+      end
+      matches
     end
 
   end
